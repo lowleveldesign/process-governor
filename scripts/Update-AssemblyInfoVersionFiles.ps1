f5c6f223db46d1d3fbf1f976441b7f5aae861c9f
Sebastian Solnica <uni@kmd.dk>
2016-08-30 08:47:13 +0200

    fixed version build script

--- scripts/Update-AssemblyInfoVersionFiles.ps1
+++ scripts/Update-AssemblyInfoVersionFiles.ps1
@@ -1,4 +1,4 @@
-function Update-AssemblyInfoVersionFiles
+function Update-AssemblyInfoVersionFiles ([string]$versionIdentifier)
 {
   $local:srcPath = $env:BUILD_SOURCESDIRECTORY
   $local:buildNumber = $env:BUILD_BUILDNUMBER
@@ -7,14 +7,14 @@ function Update-AssemblyInfoVersionFiles
 
   foreach ($file in $(Get-ChildItem $srcPath AssemblyInfo.cs -recurse))
   {
-    $local:r = [regex]"AssemblyFileVersion\(""([0-9]+\.[0-9]+)(\.([0-9]+|\*))+""\)"
+    $local:r = [regex]"$versionIdentifier\(""([0-9]+\.[0-9]+)(\.([0-9]+|\*))+""\)"
     $local:assemblyVersion = "0.0.0.0"
     #version replacements
     (Get-Content -Encoding utf8 $file.FullName) | % {
       $m = $r.Matches($_)
       if ($m -and $m.Success) {
         $assemblyVersion = "$($m.Groups[1].Value).$buildNumber"
-        $local:s = $r.Replace($_, "AssemblyFileVersion(""`$1.$buildNumber"")")
+        $local:s = $r.Replace($_, "$versionIdentifier(""`$1.$buildNumber"")")
         Write-Host "Change version to $s"
         $s
       } else {
@@ -24,4 +24,5 @@ function Update-AssemblyInfoVersionFiles
   }
 }
 
-Update-AssemblyInfoVersionFiles -Verbose
+Update-AssemblyInfoVersionFiles "AssemblyFileVersion" -Verbose
+Update-AssemblyInfoVersionFiles "AssemblyVersion" -Verbose
